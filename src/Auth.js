71338ce9e4bcf6f2024c9881c0b430a3738b14fa
Phil DeLong <philjdelong@gmail.com>
2020-06-24 08:08:25 -0600

    Delete commented section

--- src/Auth.js
+++ src/Auth.js
@@ -62,21 +62,6 @@ function Auth(props) {
                   />
                 </div>
               </div>
-              {/* <div className={`field ${auth === 'login' ? 'hidden' : ''}`}>
-                <label className="label" htmlFor="last_name">
-                  Last name:
-                </label>
-                <div className="control">
-                  <input
-                    id="last_name"
-                    className="input"
-                    type="text"
-                    name="last_name"
-                    value={lastName}
-                    onChange={e => setLastName(e.target.value)}
-                  />
-                </div>
-              </div> */}
               <div className="field">
                 <label className="label" htmlFor="email">
                   Email:
